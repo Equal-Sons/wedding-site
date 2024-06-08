9d6d70ebe6346ea0ff50e253c52737cacd7cd23a
Justin Kauszler <jkauszler@gmail.com>
2024-06-08 11:28:49 -0400

    build: added eslint and tsconfig toolings

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	extends: [
+		'xo',
+		'xo-typescript',
+	],
+	plugins: ['@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	rules: {
+		'@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+		'@typescript-eslint/naming-convention': 'off',
+		'@typescript-eslint/object-curly-spacing': ['error', 'always'],
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-call': 'off',
+		'capitalized-comments': 'off',
+		'arrow-parens': ['error', 'always'],
+		'no-shadow': 2,
+		'object-curly-spacing': ['error', 'always'],
+	},
+	ignorePatterns: [],
+};
+
--- netlify/functions/hello-world/hello-world.ts
+++ netlify/functions/hello-world/hello-world.ts
@@ -1,12 +1,12 @@
-import { Handler } from '@netlify/functions'
+import { type Handler } from '@netlify/functions';
 
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+	const { name = 'stranger' } = event.queryStringParameters as { name?: string };
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, ${name}!`,
-    }),
-  }
-}
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			message: `Hello, ${name}!`,
+		}),
+	};
+};
